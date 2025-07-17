71340e8d5ca7e3d5db925a62d873cddc7be6fc6a
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-07-16 22:44:05 -0300

    updated

---
+++ src/components/Contact.jsx
@@ -0,0 +1,23 @@
+import AnimatedTitle from "./AnimatedTitle";
+import Button from "./Button";
+
+const ImageClipBox = ({ src, clipClass }) => (
+  <div className={clipClass}>
+    <img src={src} />
+  </div>
+);
+
+const Contact = () => {
+  return (
+    <div id="contact" className="my-20 min-h-96 w-screen px-10">
+      <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
+        <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
+          <ImageClipBox
+            src="/img/contact-1.webp"
+            clipClass="contact-clip-path-1"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
