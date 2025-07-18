b8d35b58990a6ddb213ca1227ec0b1d94e3fbe48
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-07-17 23:38:42 -0300

    update

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,2 +1,77 @@
 import { useState, useRef } from "react";
 import { TiLocationArrow } from "react-icons/ti";
+
+export const BentoTilt = ({ children, className = "" }) => {
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, heigth } = itemRef.current.getBoudingClientRect();
+
+    const relativeX = (event.clientX - left) / width;
+    const relativeY = (event.clientY - top) / heigth;
+
+    const tiltX = (relativeY - 0.5) * 5;
+    const tiltY = (relativeX - 0.5) * -5;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)  scale3d( .95, .95, .95)`;
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  };
+
+  return (
+    <div
+      ref={itemRef}
+      className={className}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoCard = ({ src, title, description, isComingSoon }) => {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [hoverOpacity, setHoverOpacity] = useState(0);
+  const hoverButtonRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if (!hoverButtonRef.current) return;
+    const rect = hoverButtonRef.current.getBoundingClientRect();
+
+    setCursorPosition({
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    });
+  };
+
+  const handleMouseEnter = () => setHoverOpacity(1);
+  const handleMouseLeave = () => setHoverOpacity(0);
+
+  return (
+    <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="relative left-0 top-0 size-full object-cover object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <div>
+          <h1 className="bento-title special-font">title</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
